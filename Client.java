2fe11301aa7ce248caa59119f417f88ce1919d21
Evan Pascual <evanpascual@evans-imac.home>
2020-11-22 16:16:32 -0800

    server + client

--- Client.java
+++ Client.java
@@ -1,57 +1,83 @@
 import java.util.Scanner;
-
-// 2. Sends the message to the server
-
-// 3. Displays whatever the server sends back by using the same socket
-
-// 4. Displays an error message if the IP address or port number were entered incorrectly
+import java.io.*;
+import java.net.*; 
 
 public class Client{
-    public static void main(String[] args){
 
-        //initialize scanners to take in user input
+    //prompts user to enter a valid ip address until one is entered. Returns the ip addresss as a string.  
+    static String getIPAddress(){
+        
+        //initializers
         Scanner ip = new Scanner(System.in);
-        Scanner port = new Scanner(System.in);
-        Scanner message = new Scanner(System.in);
-
-        //initialize regex for input validation
+        String ipAddress = "";
+        boolean valid = false;
         String ipChecker = "(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)";
 
-        //user input for ip address
-        System.out.println("Enter the IP Address:");
-        String ipAddress = ip.nextLine();
-        while(ipAddress.matches(ipChecker) != true){ //input validation using regex
+        //loop for user input of ip address
+        while(!valid){
             System.out.println("Enter a valid IP Address:");
             ipAddress = ip.nextLine();
+            valid = ipAddress.matches(ipChecker);
         }
         
-        //user input for port number
+        return ipAddress;
+    }
+
+    //prompts user to enter a valid port number until one is entered. 
+    static int getPortNum(){
+
+        //initializers
         int portNum;
-        System.out.println("Enter the port number of the server:");
+        Scanner port = new Scanner(System.in);
+
+        //user input for port number
+        System.out.println("Enter a valid port number:");
         while(true) { //valid integer check
             try {
-                  portNum = Integer.parseInt(port.nextLine());
+                  portNum = port.nextInt();
                   while(portNum > 65535 || portNum < 1){ //valid integer check within range
-                    System.out.println("Enter valid port number: ");
-                    portNum = Integer.parseInt(port.nextLine());
+                    System.out.println("Enter valid port number:");
+                    portNum = port.nextInt();
                   }
                   break;
-            }catch (NumberFormatException e) {
-                System.out.println("Enter valid port number: ");
+            }catch (NumberFormatException e) { //throse 
+                System.out.println("Enter valid port number:");
                 continue;
             }
         }
 
-        //user input for message
-        System.out.println("Enter the message to be sent to the server:");
-        String messageToServer = message.nextLine();
+        return portNum;
+    }
 
-        //CHECKS INPUT. DELETE LATER
-        System.out.println("IP Address: " + ipAddress + "\nPort Number: " + portNum + "\nMessage: " + messageToServer);
-        
-        //close the scanners
-        ip.close();
-        port.close();
-        message.close();
+    public static void main(String[] args) throws UnknownHostException {
+
+        //initialize ip and host
+        InetAddress ipAddress = InetAddress.getByName(getIPAddress());
+        int portNum = getPortNum();
+
+        //Creating Socket
+        try (
+            Socket echoSocket = new Socket(ipAddress, portNum);
+            PrintWriter out =
+                new PrintWriter(echoSocket.getOutputStream(), true);
+            BufferedReader in =
+                new BufferedReader(
+                    new InputStreamReader(echoSocket.getInputStream()));
+            BufferedReader stdIn =
+                new BufferedReader(
+                    new InputStreamReader(System.in))
+        ) {
+            String userInput;
+            while ((userInput = stdIn.readLine()) != null) {
+                out.println(userInput);
+                System.out.println("echo: " + in.readLine());
+            }
+        } catch (UnknownHostException e) {
+            System.err.println("UnknownHostException");
+            System.exit(1);
+        } catch (IOException e) {
+            System.err.println("IOEXception");
+            System.exit(1);
+        } 
     }
 }
